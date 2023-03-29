456bafa9486fb52d7ed77c64c3ce18a1f28a3185
maplepolis <kmlin1090@gmail.com>
2023-03-29 15:32:14 -0400

    added stable diffusion component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProfileViewComponent } from './pages/profile-view/profile-view.componen
 import { ProfileComponent } from './components/profile/profile.component';
 import { GenerationViewComponent } from './pages/generation-view/generation-view.component';
 import { StableDiffusionComponent } from './components/stable-diffusion/stable-diffusion.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,7 @@ import { StableDiffusionComponent } from './components/stable-diffusion/stable-d
     GenerationViewComponent,
     StableDiffusionComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
