7d0cae9cf81298a6d22272f24bdb32e7871e063f
Kai Lin <85081785+maplepolis@users.noreply.github.com>
2023-03-30 03:50:04 -0400

    added SD component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,7 +14,10 @@ import { ProfileViewComponent } from './pages/profile-view/profile-view.componen
 import { ProfileComponent } from './components/profile/profile.component';
 import { GenerationViewComponent } from './pages/generation-view/generation-view.component';
 import { GenerationPreviewComponent } from './components/generation-preview/generation-preview.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { SdGenerationComponent } from './components/sd-generation/sd-generation.component';
+import { SdViewComponent } from './pages/sd-view/sd-view.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -29,13 +32,10 @@ import { ReactiveFormsModule } from '@angular/forms';
     ProfileComponent,
     GenerationViewComponent,
     GenerationPreviewComponent,
+    SdGenerationComponent,
+    SdViewComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
