58e09a5ca5e60bd193b753fbd6a982fb1521f204
Kai Lin <85081785+maplepolis@users.noreply.github.com>
2023-04-03 22:04:27 -0400

    test

--- server/services/ResumeService.py
+++ server/services/ResumeService.py
@@ -4,7 +4,6 @@ from server import app, oauth, env, meilisearchService
 from azure.core.exceptions import ResourceNotFoundError
 from server.errors.ResumeNotFoundError import ResumeNotFoundError
 from server.errors.ServerError import ServerError
-from meilisearch.errors import MeiliSearchApiError
 from celery.result import AsyncResult
 from server.services.Worker import addResume, setTaskId, getApp
 import io, base64, uuid
@@ -35,7 +34,7 @@ class ResumeService:
         try:
             resume = meilisearchService.index('resumes').get_document(id)._Document__doc
             return resume
-        except MeiliSearchApiError:
+        except Exception:
             raise ResumeNotFoundError('Resume not found in meilisearch')
 
     def parseResumeResults(self, results):
@@ -86,5 +85,5 @@ class ResumeService:
             worker = getApp()
             result = AsyncResult(document['task'], app=worker)
             return result.state
-        except MeiliSearchApiError:
+        except Exception:
             raise ServerError('Task not found, try again later')
